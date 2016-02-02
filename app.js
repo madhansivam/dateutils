fc6c31fb13efb766acd38a7c85c49f89994d7975
Madhan Sadasivam <msadasivam@apigee.com>
2016-02-02 17:50:24 +0530

    debug

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.get('/v1/now', function (req, res) {
 app.get('/v1/sleep', function (req, res) {
     var x = req.query.seconds || 10;
     if(x > 600) x = 600;
-    sleep.sleep(x);
-    res.send('Slept for ' + x + 'seconds.');
+    sleep.sleep(150);
+    res.send('Slept for ' + x + ' seconds.');
 });
 
 var server = app.listen(app.get('port'), function () {
